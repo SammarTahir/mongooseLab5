05c3e0e0cb8a5360a94e7c436fd91fd2ee31223f
SammarTahir <G00347526@gmit.ie>
2018-11-08 14:04:38 +0000

    Delete function being called

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -56,6 +57,9 @@ app.get('/api/posts', function (req, res) {
 
 })
 
+app.delete('/api/posts/:id', function(req, res){
+    console.log(req.params.id);
+})
 
 var server = app.listen(8081, function () {
     var host = server.address().address
